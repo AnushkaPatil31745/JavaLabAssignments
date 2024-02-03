fdf41650cdd50a8c7641bbca5ca8ccefd922eb08
AnushkaPatil31745 <156602958+AnushkaPatil31745@users.noreply.github.com>
2024-02-03 23:23:48 +0530

    Update ProgrammingAssignment1.java

--- ProgrammingAssignment1.java
+++ ProgrammingAssignment1.java
@@ -1,42 +1,72 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class Main2023CSE001 {
+public class 2022BIT018 {
 
-    public static void main(String[] args) {
-        String[] registrationNumbers = {
-            "2023BCS001",
-            "2023BIT002",
-            "2023BCH003",
-            "2023BME004",
-            "2023BEL005"
-        };
-
-        int validCount = getValidRegistrationsCount(registrationNumbers);
-        System.out.println("Count of valid registration numbers: " + validCount);
-    }
-
-    public static int getValidRegistrationsCount(String[] registrationNumbers) {
-        List<String> validDepartments = Arrays.asList("BCS", "BIT", "BCH", "BME");
-        int validCount = 0;
-
-        for (String regNumber : registrationNumbers) {
-            if (isValidRegistrationNumber(regNumber, validDepartments)) {
-                validCount++;
-            }
-        }
-
-        return validCount;
-    }
-
-    private static boolean isValidRegistrationNumber(String regNumber, List<String> validDepartments) {
-        if (regNumber.length() != 10) {
-            // Invalid length
-            return false;
-        }
-
-        String department = regNumber.substring(4, 7);
-
-        return validDepartments.contains(department);
-    }
+public int getValidRegistrationsCount(String []enroll)
+{
+	int count =0;
+	boolean isValid =true;
+	if(enroll == null)
+	{
+		return -1;
+	}
+	for(int i=0;i<enroll.length;i++)
+	{
+		isValid =true;
+		if(enroll[i] == null)
+		{
+			continue;
+		}
+		if(enroll[i].length() != 10)
+		{
+			continue;
+		}
+		else
+		{
+			for(int j=0;j<enroll[i].length();j++)
+			{
+				if(j < 4 || (j >= 7 && j < enroll[i].length()))
+				{
+					char charAtInd = enroll[i].charAt(j);
+					if(!Character.isDigit(charAtInd))
+					{	
+						isValid = false;
+						break;
+					}
+				}
+				else
+				{
+					Integer year = Integer.parseInt(enroll[i].substring(0, 4));
+					if(year < 2000 || year >=2024)
+					{
+						isValid = false;
+						break;
+					}
+				}
+				if(j >= 4 && j < 7)
+				{
+					String departmentId = enroll[i].substring(4,7);
+					if(!(departmentId.equalsIgnoreCase("bit") || departmentId.equalsIgnoreCase("bcs") || departmentId.equalsIgnoreCase("bme") || departmentId.equalsIgnoreCase("bch")))
+					{
+						isValid = false;
+						break;
+					}
+			       }
+			}
+			if(isValid)
+			{
+				count++;
+			}
+		}
+	}	
+	return count;
+}
+public static void main(String[] args)
+{
+	String []enrollment = {null,"2023Bit018","1999bit018","2025bcs018"};
+	SY2023bit018 data = new SY2023bit018();
+	int count  =data.getValidRegistrationsCount(enrollment);
+	System.out.println(count);
+}
 }
